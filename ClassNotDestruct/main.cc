fdcb5d4544d49bf69e031a21e332b86949c7f8a2
zouguowei <guowzou@gmail.com>
2023-09-08 10:58:29 +0800

    add class not destruct example

---
+++ ClassNotDestruct/main.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class ClassNotDestruct {
+ public:
+  ClassNotDestruct() {
+    std::cout << "construct" << std::endl;
+  }
+
+  ~ClassNotDestruct() {
+    std::cout << "destruct" << std::endl;
+  }
+
+  void Print() {
+    std::cout << "Hello world" << std::endl;
+  }
+};
+
+int main(int argc, char* argv[]) { 
+    char buffer[100];
+
+    ClassNotDestruct* p = new (buffer)ClassNotDestruct();
+    p->Print();
+    p->~ClassNotDestruct();
+    
+    return 0; 
+}
